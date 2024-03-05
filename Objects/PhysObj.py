90a9095bf96e88108f8d673221f221758d6301fe
Elly <eric.poore@gmail.com>
2024-03-04 20:46:20 -0600

    Added gravity and collision handling

--- Objects/PhysObj.py
+++ Objects/PhysObj.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 
+# Code based off of: https://www.petercollingridge.co.uk/tutorials/pygame-physics-simulation/
 class PhysObj():
     # Constructor
     # x y: coordinates
@@ -15,8 +16,10 @@ class PhysObj():
     # Moves Object
     # Speed: Pixels to move per call
     def move(self):
+        self.angle, self.speed = addVectors(self.angle, self.speed, math.pi, 0.002)
         self.x += math.sin(self.angle) * self.speed
         self.y -= math.cos(self.angle) * self.speed
+        self.speed *= 0.999
 
     # Causes obj to bounce
     # Width: screen width
@@ -27,31 +30,60 @@ class PhysObj():
         if self.x > width - size:
             self.x = 2 * (width - size) - self.x
             self.angle = -self.angle
+            self.speed *= 0.75
         elif self.x < size:
             self.x = 2* size - self.x
             self.angle = -self.angle
+            self.speed *= 0.75
 
         if self.y > height - size:
             self.y = 2 * (height - size) - self.y
             self.angle = math.pi - self.angle
+            self.speed *= 0.75
         elif self.y < size:
             self.y = 2 * size - self.y
             self.angle = math.pi - self.angle
+            self.speed *= 0.75  
+
+def collide(obj1, obj2):
+    dx = obj1.x - obj2.x
+    dy = obj1.y - obj2.y 
+
+    distance = math.hypot(dx, dy)
+    if distance < obj1.size + obj2.size:
+        tangent = math.atan2(dy, dx)
+        obj1.angle = 2 * tangent - obj1.angle
+        obj2.angle = 2 * tangent - obj2.angle
+
+        obj1.speed, obj2.speed = obj2.speed, obj1.speed
+        obj1.speed *= 0.75
+        obj2.speed *= 0.75
+
+        angle = 0.5 * math.pi + tangent
+        obj1.x += math.sin(angle)
+        obj1.y -= math.cos(angle)
+        obj2.x -= math.sin(angle)
+        obj2.y += math.cos(angle)
+
+def addVectors(ang1, len1, ang2, len2):
+    x = math.sin(ang1) * len1 + math.sin(ang2) * len2
+    y = math.cos(ang1) * len1 + math.cos(ang2) * len2 
+    length = math.hypot(x, y)
+    angle = 0.5 * math.pi - math.atan2(y, x)
+    return (angle, length)
 
 # Remove/move to a separate file eventually
 # Meant to test PhysObj class
 import random
 class TestObj(PhysObj):
-    def __init__(self, screen, x, y, weight, radius) -> None:
+    def __init__(self, screen, x, y, weight, size) -> None:
         self.screen = screen
         super().__init__(x, y, random.uniform(0, math.pi*2), 2, weight)
-        self.radius = radius
+        self.size = size
         self.color = (0, 0, 255)
 
     def draw(self):
-        super().move()
-        super().bounce(1280, 720, self.radius)
-        pygame.draw.circle(self.screen, self.color, (int(self.x), int(self.y)), self.radius)
+        pygame.draw.circle(self.screen, self.color, (int(self.x), int(self.y)), self.size)
 
     def toString(self) -> str:
-        return f"({self.x}, {self.y}) Weight: {self.weight} Radius: {self.radius}"
+        return f"({self.x}, {self.y}) Weight: {self.weight} Radius: {self.size}"
--- PhysTest.py
+++ PhysTest.py
@@ -1,7 +1,8 @@
 import asyncio
 import pygame
 import random
-from Objects.PhysObj import TestObj
+import math
+from Objects.PhysObj import TestObj, collide
 
 backgroundColor = (255, 255, 255)
 (width, height) = (1280, 720)
@@ -12,6 +13,7 @@ async def main():
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     screen.fill(backgroundColor)
+    selectedObj = None
 
     for _ in range(20):
         size = random.randint(20, 30)
@@ -23,16 +25,42 @@ async def main():
 
     running = True
     while running:
-        # exit handler
         for event in pygame.event.get():
+            # Exit handler
             if event.type == pygame.QUIT:
                 running = False
+            # Check for mouse input
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX, mouseY = pygame.mouse.get_pos()
+                selectedObj = findObject(objList, mouseX, mouseY)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                selectedObj = None
+        
+        # Move Particle
+        if selectedObj:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            dx = mouseX - selectedObj.x
+            dy = mouseY - selectedObj.y
+            selectedObj.angle = math.atan2(dy, dx) + 0.5 * math.pi
+            selectedObj.speed = math.hypot(dx, dy) * 0.1
+            selectedObj.x = mouseX
+            selectedObj.y = mouseY
 
         screen.fill(backgroundColor)
-        for obj in objList:
+        for i, obj in enumerate(objList):
+            obj.move()
+            obj.bounce(1280, 720, obj.size)
+            for obj2 in objList[i+1:]:
+                collide(obj, obj2)
             obj.draw()
         pygame.display.flip()
         
         await asyncio.sleep(0)
+
+def findObject(objects, x, y):
+    for obj in objects:
+        if math.hypot(obj.x-x, obj.y-y) <= obj.size:
+            return obj
+    return None
 
 asyncio.run(main())
