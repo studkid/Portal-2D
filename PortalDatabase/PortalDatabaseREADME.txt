c516b37e2eb419f368246c6719e581d447970f9e
Dominic <30577026+Ztel@users.noreply.github.com>
2024-03-15 03:48:02 -0500

    Created a mysql database + readme for some basic info

---
+++ PortalDatabase/PortalDatabaseREADME.txt
@@ -0,0 +1,34 @@
+----------------------------------------------
+v   Database Structure   v
+----------------------------------------------
+PK = Primary Key
+FK = Foreign Key
+
+Table GameUser:
+  int userID (PK)
+  string username  <-- plaintext username
+  string password  <-- hashed password
+
+Table Level:
+  int levelID (PK)
+  string levelName
+  int targetTime  <-- # of seconds to beat to recieve a star/medal
+
+Table LevelTime: (Intersection table)
+  int userID (FK PK)
+  int levelID (FK PK)
+  int completionTime  <-- # of seconds it took to beat the level
+
+----------------------------------------------
+v   Implementation Tips   v
+----------------------------------------------
+Users:
+User data needs to be stored in the database, but passwords should not be exposed, because that is insecure. As a base level of security, the password should be hashed when the user creates their account. The username and hashed password is then stored in a new row of the GameUser table. When a user logs in, the input password should be hashed and compared against the hash stored in the database. If they match, the user logs in. If not, then they should be denied.
+
+Levels:
+There will need to be some correlation between how the levels are loaded in-game and how they are stored in the database. My suggestion is to store level data as text. Each level would be a .txt file with a grid of characters. Different characters represents a different object, such as an X for a non-portal-able tile, O for a portal-able tile, a space for an empty tile, etc. This would make creating, storing, and editing levels much easier than manually writing the position of every single tile in an array. Additionally, I could create a dev tool for us to view and design levels that would let you click to place tiles, and then export a file with the raw level data.
+
+Updating Level Completion Times:
+Both players need to have their completion times updated after they complete a level. 
+First, check if there is an existing row in the LevelTime table for the current userID AND levelID. If there is, AND the new completion time is faster than the existing one, update that row with the new completionTime. If not, create a new row with the current userID, levelID, and completionTime.
+
---
+++ PortalDatabase/portalgame.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2024 at 03:20 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portalgame`
+--
+CREATE DATABASE IF NOT EXISTS `portalgame` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `portalgame`;
+
+DROP TABLE IF EXISTS `leveltime`;
+DROP TABLE IF EXISTS `gameuser`;
+DROP TABLE IF EXISTS `level`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gameuser`
+--
+
+CREATE TABLE IF NOT EXISTS `gameuser` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE IF NOT EXISTS `level` (
+  `levelID` int(11) NOT NULL AUTO_INCREMENT,
+  `levelName` varchar(100) DEFAULT NULL,
+  `targetTime` int(11) DEFAULT NULL,
+  PRIMARY KEY (`levelID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leveltime`
+--
+
+CREATE TABLE IF NOT EXISTS `leveltime` (
+  `userID` int(11) NOT NULL,
+  `levelID` int(11) NOT NULL,
+  `completionTime` int(11) DEFAULT NULL,
+  KEY `userID_FK` (`userID`),
+  KEY `levelID_FK` (`levelID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `leveltime`
+--
+ALTER TABLE `leveltime`
+  ADD CONSTRAINT `levelID_FK` FOREIGN KEY (`levelID`) REFERENCES `level` (`levelID`),
+  ADD CONSTRAINT `userID_FK` FOREIGN KEY (`userID`) REFERENCES `gameuser` (`userID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
