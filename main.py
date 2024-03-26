4f4966398ca8d637f12fbf974dd464b853d9b5c7
jonleitz <amazingspidercat69@gmail.com>
2024-03-26 15:43:22 -0500

    Add sign up/log off

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pygame
 import asyncio
 from typing import Dict
 from Utils.MenuButton import MenuButton
-import test_code
 import connection
 import levels
+import test_code
+import signup
 from Utils import GlobalVariables
 
 background = pygame.Surface((GlobalVariables.Width, GlobalVariables.Height))
@@ -25,6 +26,15 @@ buttons: Dict[str, MenuButton] = {
 }
 
 async def main():
+    
+    sign_up_button = MenuButton(GlobalVariables.Width - 150, 50, "Sign up", GlobalVariables.font(24), GlobalVariables.Text_Forecolor, GlobalVariables.Text_Hovercolor)
+    log_off_button = MenuButton(GlobalVariables.Width - 150, 90, "Log off", GlobalVariables.font(24), GlobalVariables.Text_Forecolor, GlobalVariables.Text_Hovercolor)
+
+    sign_up_button.rect.right = GlobalVariables.Width - 50
+    log_off_button.rect.right = GlobalVariables.Width - 50
+
+    logged = GlobalVariables.Account_Username is not ""
+
     while True:
         screen.blit(background, (0,0))
 
@@ -35,7 +45,19 @@ async def main():
         title_text = GlobalVariables.font(50).render("Portal 2D", True, GlobalVariables.Text_Forecolor)
         title_rect = pygame.Rect(50, 50, title_text.get_width(), title_text.get_height())
 
+        user_text = GlobalVariables.font(30).render("Welcome, " + GlobalVariables.Account_Username, True, GlobalVariables.Text_Forecolor)
+        user_rect = user_text.get_rect(right=GlobalVariables.Width - 50, top=50)
+
         screen.blit(title_text, title_rect)
+
+        if logged:
+            screen.blit(user_text, user_rect)
+            log_off_button.check_hover(mouse_pos)
+            log_off_button.update(screen)
+
+        else:
+            sign_up_button.check_hover(mouse_pos)
+            sign_up_button.update(screen)
 
         for key in buttons:
             buttons[key].check_hover(mouse_pos)
@@ -54,6 +76,13 @@ async def main():
                 if buttons["test_button"].check_click(mouse_pos):
                     await test_code.test_screen()
                     pygame.display.set_mode((GlobalVariables.Width,GlobalVariables.Height))
+                if sign_up_button.check_click(mouse_pos):
+                    await signup.sign_up()
+                    pygame.display.set_mode((GlobalVariables.Width,GlobalVariables.Height))
+                if log_off_button.check_click(mouse_pos):
+                    GlobalVariables.Account_ID = ""
+                    GlobalVariables.Account_Username = ""
+                    logged = False
 
         pygame.display.update()
 
---
+++ signup.py
@@ -0,0 +1,28 @@
+import pygame
+import asyncio
+from Utils import GlobalVariables
+
+background = pygame.Surface((GlobalVariables.Width, GlobalVariables.Height))
+background.fill(GlobalVariables.Background_Color)
+
+pygame.display.set_caption("Portal 2D - Sign up")
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((GlobalVariables.Width, GlobalVariables.Height))
+
+async def sign_up():
+    running = True
+    while running:
+        screen.blit(background, (0,0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+
+        await asyncio.sleep(0)
