b183fa64dba8ecb8db5e5ea1d9d84b5f0d26d01f
Elly <eric.poore@gmail.com>
2024-03-29 17:24:25 -0500

    CubeDropper added

--- Test/PhysTest.py
+++ Test/PhysTest.py
@@ -8,15 +8,16 @@ import os
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from Utils.PhysObj import PhysObj, CubeObj
+from Utils.PhysObj import CubeObj
 from Utils.ButtonObject import ButtonObject
 from Utils.Platform import Platform
+from Utils.CubeDropper import CubeDropper
 
 backgroundColor = (255, 255, 255)
 plaformColor = (41, 41, 41)
 (width, height) = (1280, 720)
 
-objGroup = pygame.sprite.Group()
+# dropper = pygame.sprite.Group()
 wallList = [
     Platform(0, height - 20, width, 20, True, None),
     Platform(200, 300, 100, 20, True, None),
@@ -51,13 +52,9 @@ async def PhysTest():
     selectedObj = None
     clock = pygame.time.Clock()
 
-    for _ in range(1):
-        size = random.randint(40, 50)
-        x = random.randint(size, width-size)
-        y = random.randint(size, height-size)
-        print(f"{x} {y}")
-        # objList.append(TestObj(x, y, size, 0.0999, 0.2))
-        objGroup.add(CubeObj(x, y, 0.0999, 0.2))
+    dropper = CubeDropper(1160, -15, 135)
+    dropper.add(CubeObj(0, 0, 0.0999, 0.2))
+    dropper.spawnCube()
 
     button = ButtonObject(230, 285, 0)
     pygame.display.update()
@@ -78,9 +75,10 @@ async def PhysTest():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouseX, mouseY = pygame.mouse.get_pos()
                 if event.button == 1:
-                    selectedObj = findObject(objGroup.sprites(), mouseX, mouseY)
+                    selectedObj = findObject(dropper.sprites(), mouseX, mouseY)
                 elif event.button == 3:
-                    objGroup.add(CubeObj(mouseX, mouseY, 0.0999, 0.2))
+                    dropper.spawnCube()
+                    print(dropper.rect.center)
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     selectedObj = None
@@ -98,15 +96,15 @@ async def PhysTest():
 
         screen.fill(backgroundColor)
 
-        for i, obj in enumerate(objGroup.sprites()):
+        for i, obj in enumerate(dropper.sprites()):
             if obj != selectedObj:
                 obj.move(dt)
             obj.bounce(1280, 720, wallList)
-            obj.collide(objGroup.sprites()[i+1:])
-        objGroup.update()
-        objGroup.draw(screen)
+            obj.collide(dropper.sprites()[i+1:])
+        dropper.update()
+        dropper.draw(screen)
 
-        button.checkActive(objGroup.sprites())
+        button.checkActive(dropper.sprites())
         button.draw(screen)
         
         for wall in wallList:
