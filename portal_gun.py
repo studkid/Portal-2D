bcfdd9518560b8854879cc2a14b2dcefd1353f62
Dogmaa <106295274+Dogmaa@users.noreply.github.com>
2024-04-08 14:53:03 -0500

    Update portal_gun.py

--- portal_gun.py
+++ portal_gun.py
@@ -3,15 +3,18 @@ from sys import exit
 import math
 from game_settings import *
 
+
 pygame.init()
+
+#platforms
+platform = pygame.Rect( 100,300,200,50 )
+
 
 #tab/window settings
 screen = pygame.display.set_mode( ( WIDTH, HEIGHT ) )
 pygame.display.set_caption( "2D Portal" )
 clock = pygame.time.Clock()
 
-#loads images
-background = pygame.transform.scale(pygame.image.load( "img/background/background.png" ).convert(), ( WIDTH, HEIGHT ) )
 #portal gun  
 class Pgun( pygame.sprite.Sprite ):
     def __init__( self ):
@@ -24,8 +27,8 @@ class Pgun( pygame.sprite.Sprite ):
         self.speed = PLAYER_SPEED #reminder to change to lock on to player
         self.shoot = False
         self.shoot_cooldown = 0
-        self.gun_barrel_offset = pygame.math.Vector2( GUN_OFFSET_X, GUN_OFFSET_Y ) #sets how far away the portal gun is away from the player
-       
+        self.gun_barrel_offset = pygame.math.Vector2( 0, 0 ) #sets how far away the portal gun is away from the player
+      
 #locks aims the pgun twords wherever the mouse is 
     def pgun_rotation( self ):
         self.mouse_coords = pygame.mouse.get_pos()
@@ -44,14 +47,17 @@ class Pgun( pygame.sprite.Sprite ):
 #wasd movment used for testing
 #will change so the portal gun follows the player
         keys = pygame.key.get_pressed()
-        #if keys[ pygame.K_w ]:
-            #self.velocity_y = -self.speed
-        #if keys[ pygame.K_s ]:
-            #self.velocity_y = self.speed
-        #if keys[ pygame.K_d ]:
-            #self.velocity_x = self.speed
-        #if keys[ pygame.K_a ]:
-            #self.velocity_x = -self.speed
+        if keys[ pygame.K_w ]:
+            self.velocity_y = -self.speed
+        if keys[ pygame.K_s ]:
+            self.velocity_y = self.speed
+        if keys[ pygame.K_d ]:
+            self.velocity_x = self.speed
+        if keys[ pygame.K_a ]:
+            self.velocity_x = -self.speed
+    #bullet test code
+        if keys[ pygame.K_h ]:
+            BULLET_LIFETIME = 0
 
 #used for testing diagonal movemnt
         #if self.velocity_x != 0 and self.velocity_y != 0:
@@ -63,6 +69,7 @@ class Pgun( pygame.sprite.Sprite ):
             self.is_shooting()
         else:
             self.shoot = False
+        
 
 #sets a delay when you shoot
     def is_shooting( self ): 
@@ -72,6 +79,7 @@ class Pgun( pygame.sprite.Sprite ):
             self.bullet = Bullet( spawn_bullet_pos[0], spawn_bullet_pos[1], self.angle )
             bullet_group.add( self.bullet )
             all_sprites_group.add( self.bullet )
+
             
 #moves hitbox
     def move( self ):
@@ -103,6 +111,8 @@ class Bullet( pygame.sprite.Sprite ):
         self.y_vel = math.sin( self.angle * ( 2*math.pi/360 ) ) * self.speed 
         self.bullet_lifetime = BULLET_LIFETIME
         self.spawn_time = pygame.time.get_ticks() #gets the time that the bullet was created
+        self.collide = False
+
 
     def bullet_movement( self ):  
         self.x += self.x_vel
@@ -113,9 +123,61 @@ class Bullet( pygame.sprite.Sprite ):
 
         if pygame.time.get_ticks() - self.spawn_time > self.bullet_lifetime: #despawn bullet if it goes to far
             self.kill() 
+        
+        if self.rect.colliderect(platform):
+            self.collide = True
+            self.bullet_col()
+            #test code
+            #print( 'nice' )
+        else:
+            self.collide = False
+
+ #gets rid of bullet image and spawns portal
+    def bullet_col( self ):
+        spawn_portal_pos =  self.x, self.y
+        self.bullet = Portal( spawn_portal_pos[0], spawn_portal_pos[1], self.angle )
+        bullet_group.add( self.bullet )
+        all_sprites_group.add( self.bullet )
+        self.kill() 
+        self.collide = False
+      
 
     def update( self ):
         self.bullet_movement()
+        
+        
+class Portal( pygame.sprite.Sprite ):
+    def __init__( self, x, y, angle ):
+        super().__init__()
+        self.image = pygame.image.load( "img/Portal_Blue.png" ).convert_alpha()
+        self.image = pygame.transform.rotozoom( self.image, 0, PORTAL_SCALE ) #adjust portal size
+        self.rect = self.image.get_rect()
+        self.rect.center = ( x, y )
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.spawn_time = pygame.time.get_ticks()
+        self.spawned_portals = SPAWNED_PORTALS
+
+
+    def portal_count( self ):
+        if pygame.mouse.get_pressed() == ( 1, 0, 0 ): #( 1, 0, 0 ) means left click
+            self.spawned_portals = self.spawned_portals + 1
+
+
+    def max_portals( self ):
+        
+        if self.spawned_portals == 2:
+            self.kill()
+            self.spawned_portals = SPAWNED_PORTALS
+            print( 'iwork' )
+    
+
+
+
+        
+
+
 
 pgun = Pgun()
 
@@ -123,20 +185,6 @@ all_sprites_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 
 all_sprites_group.add( pgun )
-
-
-
-while True:
-    keys = pygame.key.get_pressed()
-
-#lets you quit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    screen.blit( background, ( 0, 0 ) )
-
 
     all_sprites_group.draw(screen)
     all_sprites_group.update()
