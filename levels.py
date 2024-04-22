b3be055651c857b4cdd2949eab7cb80808f72174
Jon Leitzke <158106309+jonleitz@users.noreply.github.com>
2024-04-22 11:15:17 -0500

    added printLevels()

--- levels.py
+++ levels.py
@@ -2,6 +2,7 @@ import pygame
 import asyncio
 import account
 from Utils import GlobalVariables
+from PortalDatabase import DatabaseUtil
 
 background = pygame.Surface((GlobalVariables.Width, GlobalVariables.Height))
 background.fill(GlobalVariables.Background_Color)
@@ -10,9 +11,19 @@ pygame.display.set_caption("Portal 2D - Levels")
 
 pygame.init()
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((GlobalVariables.Width, GlobalVariables.Height))
+screen = pygame.display.set_mode((GlobalVariables.Width, GlobalVariables.Height)) 
 
 async def level_screen():
+
+    def printLevels():
+        levels = DatabaseUtil.get_levels()
+        for i in range (len(levels)):
+            levelId = GlobalVariables.font(28).render("Level " + str(levels[i][0]), True, GlobalVariables.Text_Forecolor)
+            levelId_Rect = pygame.Rect(100, 100, levelId.get_width(), levelId.get_height())
+            levelName = GlobalVariables.font(28).render(str(levels[i][1]), True, GlobalVariables.Text_Forecolor)
+            levelName_Rect = pygame.Rect(100, 100, levelName.get_width(), levelName.get_height())
+            levelTarget = GlobalVariables.font(28).render("Target time : " + str(levels[i][2]), True, GlobalVariables.Text_Forecolor)
+            levelTarget_Rect = pygame.Rect(100, 100, levelTarget.get_width(), levelTarget.get_height())
 
     if GlobalVariables.Account_Username == "":
         running = False
@@ -27,6 +38,8 @@ async def level_screen():
         title_rect = pygame.Rect(50, 50, title_text.get_width(), title_text.get_height())
 
         screen.blit(title_text, title_rect)
+
+        printLevels()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
