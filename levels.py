80f932a63e070ab7dfe2ee15b34c18cb1a8abfc2
Jon Leitzke <158106309+jonleitz@users.noreply.github.com>
2024-04-22 11:52:29 -0500

    Update printLevels()

--- levels.py
+++ levels.py
@@ -17,13 +17,50 @@ async def level_screen():
 
     def printLevels():
         levels = DatabaseUtil.get_levels()
+        user_times = DatabaseUtil.get_level_times(GlobalVariables.Account_Username)
+
+        user_has_completion = user_times is not None
+
+        
+        block_width = 300 
+        block_height = 100 
+        padding = 30 
+        start_x = 100 
+        start_y = 150
+        x = start_x
+        y = start_y
+
         for i in range (len(levels)):
-            levelId = GlobalVariables.font(28).render("Level " + str(levels[i][0]), True, GlobalVariables.Text_Forecolor)
-            levelId_Rect = pygame.Rect(100, 100, levelId.get_width(), levelId.get_height())
-            levelName = GlobalVariables.font(28).render(str(levels[i][1]), True, GlobalVariables.Text_Forecolor)
-            levelName_Rect = pygame.Rect(100, 100, levelName.get_width(), levelName.get_height())
-            levelTarget = GlobalVariables.font(28).render("Target time : " + str(levels[i][2]), True, GlobalVariables.Text_Forecolor)
-            levelTarget_Rect = pygame.Rect(100, 100, levelTarget.get_width(), levelTarget.get_height())
+
+            levelInfo = GlobalVariables.font(24).render("Level " + str(levels[i][0]) + " | " + str(levels[i][1]), True, GlobalVariables.Text_Forecolor)
+            levelInfo_Rect = pygame.Rect(x, y, levelInfo.get_width(), levelInfo.get_height())
+
+            y += padding
+
+            levelTarget = GlobalVariables.font(24).render("Target time : " + str(levels[i][2]) + " Minutes | ", True, GlobalVariables.Text_Forecolor)
+            levelTarget_Rect = pygame.Rect(x, y, levelTarget.get_width(), levelTarget.get_height())
+            
+            x += 320 + padding
+
+            if user_has_completion:
+                try:
+                    if user_times[i][2] is not None:
+                        userTimeCompleted = GlobalVariables.font(24).render("Completed in : " + str(user_times[i][2]) + " Minutes", True, GlobalVariables.Text_Forecolor)
+                    else:
+                        userTimeCompleted = GlobalVariables.font(24).render("Not completed", True, GlobalVariables.Text_Forecolor)
+                except IndexError:
+                    userTimeCompleted = GlobalVariables.font(24).render("Not completed", True, GlobalVariables.Text_Forecolor)
+            else:
+                userTimeCompleted = GlobalVariables.font(24).render("Not completed", True, GlobalVariables.Text_Forecolor)
+            
+            userTimeCompleted_Rect = pygame.Rect(x, y, userTimeCompleted.get_width(), userTimeCompleted.get_height())
+
+            screen.blit(levelInfo, levelInfo_Rect)
+            screen.blit(levelTarget, levelTarget_Rect)
+            screen.blit(userTimeCompleted, userTimeCompleted_Rect)
+
+            y += 80
+            x -= (320 + padding)
 
     if GlobalVariables.Account_Username == "":
         running = False
