9a0feb7a82a355c99e301bb37bd6c71f1d77c8f0
Elly <eric.poore@gmail.com>
2024-04-26 16:54:05 -0500

    Added transparency to special walls

--- Utils/Platform.py
+++ Utils/Platform.py
@@ -10,7 +10,10 @@ class Platform():
     #       2 = Only player
     # active: determins if the platform should be rendered/collided with
     def __init__(self, x, y, width, length, isPortable, collision):
-        self.rect = pygame.Rect(x, y, width, length)
+        self.surface = pygame.Surface((width, length))
+        self.rect = self.surface.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.isPortable = isPortable
         self.collision = collision
         self.active = True
@@ -21,12 +24,16 @@ class Platform():
             return
 
         if self.collision == 1:
-            color = (255, 0, 0)
+            self.surface.fill((255, 0, 0))
+            self.surface.set_alpha(128)
         elif self.collision == 2:
-            color = (3, 155, 229)
+            self.surface.fill((3, 155, 229))
+            self.surface.set_alpha(128)
         elif self.isPortable:
-            color = (150, 150, 150)
+            self.surface.fill((150, 150, 150))
+            self.surface.set_alpha(255)
         else:
-            color = (10, 10, 10)
+            self.surface.fill((10, 10, 10))
+            self.surface.set_alpha(255)
 
-        pygame.draw.rect(screen, color, self.rect)
+        screen.blit(self.surface, (self.rect.x, self.rect.y))
